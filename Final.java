ae78d254093302448c5d5033a43f5572c2800949
Harshitha A.B <harshithaab628@gmail.com>
2024-12-09 13:52:21 +0530

    second

---
+++ Final.java
@@ -0,0 +1,8 @@
+public class Final {
+    public static void main(String[] args) {
+      int myNum = 15;
+      System.out.println(myNum);
+      int myNum = 20;  
+      System.out.println(+ myNum);
+    }
+  }
